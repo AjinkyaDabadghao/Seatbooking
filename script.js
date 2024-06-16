bcaddc9cc66d2170ecdadd07ded4031177f48d65
Ajinkya <patilajinkya5683@gmail.con>
2024-06-16 22:53:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const container = document.querySelector('.container');
+
+container.addEventListener('click', (e) => {
+    if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+        e.target.classList.toggle('selected');
+    }
+});
